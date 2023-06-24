84a69d0f14a6f63187e35256e1253c9fd5648514
NovaRyte <85930892+NovaRyte@users.noreply.github.com>
2023-06-25 00:51:21 +1000

    h

--- Assets/Script/PlayerCharacter.cs
+++ Assets/Script/PlayerCharacter.cs
@@ -1,43 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    //need player character to attack when we press the spacebar...
-    //but first lets it to add a level
-    //Variables go here
+    // need player character to attack when spacebar is pressed
+    //but first add a level
     [SerializeField] private int attack = 10;
     [SerializeField] private int health = 100;
     [SerializeField] private int level = 1;
+    [SerializeField] private int experience = 0;
+    [SerializeField] private double exptolevelup = 50;
 
-    //Methods go here
+    public TMP_Text combatLog;
+    //methods go here
 
-    // Start is called before the first frame update
-    void Start()
+    //start is called before the first frame update
+    private void Start()
     {
-        attack = 10;
-        health = 100;
-        level = 1;
+        combatLog.text = "Beriet, KAMPFE!";
     }
 
+
+    // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Attila has hit the spacebar on his keyboard at the front of the room!");
-            level++;
-        }
+            Debug.Log("You Attack!");
+            Debug.Log("Attack: " + Mathf.Round((float)attack * 100) / 100);
 
-        if(level == 5)
+        }
+        if(experience >= exptolevelup)
         {
-            Debug.Log("YOU WIN");
+            LevelUp();
+        }
+        if (level == 5)
+        {
+            combatLog.text = "You Win!";
+            Debug.Log("You Win!");
         }
     }
-    private void StartTheGameAgainFromLevelOne()
+
+    private void LevelUp()
     {
-        attack = 10;
-        health = 100;
-        level = 1;
+        attack = (int)(attack * 1.225);
+        level++;
+        exptolevelup *= 1.45;
+        combatLog.text = "You Leveled Up!";
+        Debug.Log("You Leveled Up!");
     }
 }
